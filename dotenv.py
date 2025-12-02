7ae9f052d217f7b8eaaf5f84ef7bb34dfb8ebacb
Hakima-Muktar <hakimamuktar73@gmail.com>
2025-12-02 09:00:48 +0300

    commit

--- dotenv.py
+++ dotenv.py
@@ -1,17 +1,13 @@
-import os
 from dotenv import load_dotenv
-load_dotenv()
-def get_env(key: str, default: str = None) -> str:
-    value = os.getenv(key, default)
-    if value is None:
-        print(f"[WARNING] Environment variable '{key}' not found.")
-    return value
-if __name__ == "__main__":
-    PROXY_URL = get_env("PROXY_URL")
-    OUTPUT_FOLDER = get_env("OUTPUT_FOLDER", "./results")
-    EMAIL_NOTIFICATION = get_env("EMAIL_NOTIFICATION")
+import os
+from sqlalchemy import create_engine
 
-    print("Play Store Scraper Environment Loaded:")
-    print(f"PROXY_URL: {PROXY_URL}")
-    print(f"OUTPUT_FOLDER: {OUTPUT_FOLDER}")
-    print(f"EMAIL_NOTIFICATION: {EMAIL_NOTIFICATION}")
+load_dotenv()
+
+host = os.getenv("localhost")
+port = os.getenv("5432")
+db = os.getenv("mobilebank")
+user = os.getenv("hakima")
+password = os.getenv("123")
+
+engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
